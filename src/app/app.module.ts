cd47005bee678aebe4ab696ba5ec2d21bb4e8918
Aleksander Bronowicki <abronowicki@iqsolution.pl>
2017-05-07 15:51:16 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+import { EmailComponent } from './email/email.component';
+
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    MembersComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app'
+import { Router } from '@angular/router';
+import { moveIn } from '../router.animations';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  animations: [moveIn()],
+  host: { '[@moveIn]': '' }
+})
+export class LoginComponent implements OnInit {
+
+  error: any;
+  constructor(public af: AngularFireAuth, private router: Router) {
+    this.af.authState.subscribe(auth => {
+      console.log(auth);
+      if (auth) {
+        this.router.navigateByUrl('/members');
+      }
+    });
+  }
+
+  loginFb() {
+    this.af.auth.signInWithPopup(
+      new firebase.auth.FacebookAuthProvider()
+    ).then(
+      (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+      (err) => {
+        this.error = err;
+      })
+  }
+
+  loginGoogle() {
+    this.af.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    ).then(
+      (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+      (err) => {
+        this.error = err;
+      })
+  }
+
+  ngOnInit() {
+
+  }
+
+
+}
